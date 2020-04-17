2598c24d362dc44f2ea1f2a7d8bb1e54542f9989
Nguyen, Frank <frank.nguyen@thermofisher.com>
2020-04-17 22:50:01 +0800

    feature: add importing the training set code

--- occupancy-detection.py
+++ occupancy-detection.py
@@ -6,3 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
+
+####################################################
+# IMPORTING THE TRAINING SET                       #
+####################################################
+dataset = pd.read_csv('training_set.csv')
+# signature is a set of measurements (Temperature, Humidity and CO2 used to determine the occupancy
+signature = dataset.iloc[:, :-1].values
+# occupancy is an indicator to determine if the room is occupied (0 = NO and 1 = YES)
+occupancy = dataset.iloc[:, -1].values
+
