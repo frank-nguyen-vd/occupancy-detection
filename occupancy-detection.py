ac17676a8906717fbbd2e6584d7f8cd01d66b42c
Nguyen, Frank <frank.nguyen@thermofisher.com>
2020-04-17 22:48:33 +0800

    feature: import necessary libraries such as numpy, matplotlib, pandas, sklearn

---
+++ occupancy-detection.py
@@ -0,0 +1,8 @@
+####################################################
+# IMPORTING THE LIBRARIES                          #
+####################################################
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
